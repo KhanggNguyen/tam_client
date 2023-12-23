1ff08bc53b31d7b5f329eb96234a9c556349d694
Khang <kukhang2411@gmail.com>
2023-12-23 22:43:00 +0100

    update loading message

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -4,6 +4,10 @@ export const Spinner = () => {
     return (
         <div className="spinner-container">
             <div className="loading-spinner"></div>
+            <div className="loading-message">
+                Le premier chargement peut prendre du temps, veuillez patienter,
+                s'il vous plaît...
+            </div>
         </div>
     );
 };
